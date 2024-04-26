272ced20183497ec7619873c1b9ae9742c73ee83
vansh020 <vanshdhalor04@gmail.com>
2024-04-26 12:01:25 +0530

    user

---
+++ user status/user.js
@@ -0,0 +1,31 @@
+
+function show(){
+  alert("this show your intenet status you are online or offline");
+}
+
+
+function updateUserStatus() {
+const statusElement = document.getElementById('status');
+
+if (navigator.onLine) {
+statusElement.textContent = "user status is: Online";
+statusElement.style.backgroundColor = "green";   //if use is online color id green
+alert("your are now online👍");
+
+
+} else {
+statusElement.textContent = "user status is: Offline";
+statusElement.style.backgroundColor = "red";    //if use is offline color id green
+alert("you are offline turn on the internet connection👎🏼.");
+}
+}
+
+window.addEventListener('online', function(event) {
+updateUserStatus();
+});
+
+window.addEventListener("offline", function(event) {
+updateUserStatus();
+});
+
+updateUserStatus();
